d90fc722a11de668783a7833c0c4de68c7375f38
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2024-12-27 13:53:39 -0300

    feat: simplify dracula colorscheme configuration
    leverage defaults

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,37 +4,11 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            ---@type dracula
             local dracula = require "dracula"
+            local vim = vim
 
             dracula.setup({
-                styles = {
-                    Type = {},
-                    Function = {},
-                    Parameter = {},
-                    Property = {},
-                    Comment = {},
-                    String = {},
-                    Keyword = {},
-                    Identifier = {},
-                    Constant = {},
-                },
                 transparent = false,
-                on_colors = function(colors, color)
-                    ---@type dracula.palette
-                    return {
-                        -- override or create new colors
-                        mycolor = "#ffffff",
-                        -- mycolor = 0xffffff,
-                    }
-                end,
-                on_highlights = function(colors, color)
-                    ---@type dracula.highlights
-                    return {
-                        ---@type vim.api.keyset.highlight
-                        Normal = { fg = colors.mycolor }
-                    }
-                end,
                 plugins = {
                     ["nvim-treesitter"] = true,
                     ["nvim-lspconfig"] = true,
