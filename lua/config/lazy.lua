405b2b0882a3aecd6eb577ca6fc1ec3cef2130cc
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2024-12-27 11:05:59 -0300

    feat: add telescope and dracula plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,7 +34,10 @@ require("lazy").setup({
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "habamax" } },
+    install = { colorscheme = { "dracula-nvim" } },
+    opts = {
+        colorscheme = "dracula-nvim",
+    },
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
