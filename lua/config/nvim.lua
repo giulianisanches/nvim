faaacaf800f52d9e16396a87e586196ba35b5f1f
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2024-12-27 11:53:58 -0300

    feat: add keymaps for copy/paste from/to clipboard

--- lua/config/nvim.lua
+++ lua/config/nvim.lua
@@ -29,10 +29,18 @@ vim.keymap.set("ca", "Wq", "wq")
 vim.keymap.set("ca", "wQ", "wq")
 vim.keymap.set("ca", "Q", "q")
 
+-- windows/linux
+local meta_chr_copy = "<C-c>"
+local meta_chr_paste = "<C-v>"
+
+if vim.fn.has("mac") then
+    meta_chr_copy = "<D-c>"
+    meta_chr_paste = "<D-v>"
+end
+
+vim.keymap.set("n", meta_chr_copy, '"+y')
+vim.keymap.set("v", meta_chr_copy, '"+y')
+vim.keymap.set("n", meta_chr_paste, '"+p')
+
 vim.cmd("filetype plugin indent on")
 vim.cmd("colorscheme dracula")
--- paste from clipboard
--- nnoremap <D-v> "+p
--- nmap <c-c> "+y
--- vmap <c-c> "+y
--- nmap <c-v> "+p
