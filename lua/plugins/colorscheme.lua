c93af9027c7f8125c0416b9cc3e7a1d87a206cc4
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2024-12-27 13:47:39 -0300

    feat: replace dracula colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,54 @@
 return {
-    -- add dracula
-    { "Mofiqul/dracula.nvim" },
+    return {
+        {
+          'maxmx03/dracula.nvim',
+          lazy = false,
+          priority = 1000,
+          config = function ()
+            ---@type dracula
+            local dracula = require "dracula"
+
+            dracula.setup({
+            styles = {
+              Type = {},
+              Function = {},
+              Parameter = {},
+              Property = {},
+              Comment = {},
+              String = {},
+              Keyword = {},
+              Identifier = {},
+              Constant = {},
+            },
+            transparent = false,
+            on_colors = function (colors, color)
+              ---@type dracula.palette
+              return {
+                -- override or create new colors
+                mycolor = "#ffffff",
+                -- mycolor = 0xffffff,
+              }
+            end,
+            on_highlights = function (colors, color)
+              ---@type dracula.highlights
+              return {
+                ---@type vim.api.keyset.highlight
+                Normal = { fg = colors.mycolor }
+              }
+            end,
+            plugins = {
+              ["nvim-treesitter"] = true,
+              ["nvim-lspconfig"] = true,
+              ["nvim-cmp"] = true,
+              ["nvim-tree.lua"] = true,
+              ["gitsigns.nvim"] = true,
+              ["neogit"] = true,
+              ["todo-comments.nvim"] = true,
+              ["lazy.nvim"] = true,
+              ["telescope.nvim"] = true,
+            }
+            })
+            vim.cmd.colorscheme 'dracula'
+          end
+        },
 }
