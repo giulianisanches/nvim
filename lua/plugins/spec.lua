af880b74301e621990ed0420b88d10a715622445
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2024-12-27 10:41:27 -0300

    feat: add nvim-tree plugin

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -1,3 +1,24 @@
 return {
-    { "nvim-lualine/lualine.nvim" }
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {
+                sync_root_with_cwd = true,
+                respect_buf_cwd = true,
+                update_focused_file = {
+                    enable = true,
+                    update_root = true
+                },
+            }
+        end,
+    }
 }
