7ccda9307182a2e6967636e2b2cb42493ec6a433
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2025-01-03 15:10:33 -0300

    feat: show dotfiles, except .git

--- lua/plugins/ntree.lua
+++ lua/plugins/ntree.lua
@@ -24,7 +24,7 @@ return {
                     group_empty = true,
                 },
                 filters = {
-                    dotfiles = true,
+                    custom = { "^.git$" }
                 },
             }
         end,
