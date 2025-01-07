47cb6058097cf16e36bca0dffcde798f07c0f492
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2025-01-07 09:24:05 -0300

    fix: treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,29 +7,27 @@ return {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup {
-                options = {
-                    ensure_installed = {
-                        "c",
-                        "lua",
-                        "vim",
-                        "vimdoc",
-                        "javascript",
-                        "html",
-                        "python",
-                        "puppet",
-                        "ruby",
-                        "bash",
-                        "c_sharp",
-                        "dockerfile",
-                        "markdown",
-                    },
-                    sync_install = false,
-                    highlight = {
-                        enable = true,
-                        additional_vim_regex_highlighting = false,
-                    },
-                    indent = { enable = true },
-                }
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "javascript",
+                    "html",
+                    "python",
+                    "puppet",
+                    "ruby",
+                    "bash",
+                    "c_sharp",
+                    "dockerfile",
+                    "markdown",
+                },
+                sync_install = false,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = { enable = true },
             }
         end,
     }
