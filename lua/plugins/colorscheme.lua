1d1f8b8bd91ac5c5e17cf4ecfe191f1b74bf6206
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2025-05-09 14:49:04 -0300

    fix: switch colorscheme from dracula to solarized with updated config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,15 @@
 return {
     {
-        'maxmx03/dracula.nvim',
+        'maxmx03/solarized.nvim',
         lazy = false,
         priority = 1000,
         config = function()
-            local configs = require "dracula"
+            local configs = require("solarized")
             local vim = vim
 
-            configs.setup({
-                transparent = false,
-                plugins = {
-                    ["nvim-treesitter"] = true,
-                    ["nvim-lspconfig"] = true,
-                    ["nvim-cmp"] = true,
-                    ["nvim-tree.lua"] = true,
-                    ["gitsigns.nvim"] = true,
-                    ["neogit"] = true,
-                    ["todo-comments.nvim"] = true,
-                    ["telescope.nvim"] = true,
-                }
-            })
-            vim.cmd.colorscheme 'dracula'
+            vim.opt.background = 'dark'
+            configs.setup({ })
+            vim.cmd.colorscheme 'solarized'
         end
     },
 }
