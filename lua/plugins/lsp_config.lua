f0bfd7ad35e3d8991711cbaf4d977902c5a1cae0
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2025-06-05 10:22:35 -0300

    feat: add lsp configuration

---
+++ lua/plugins/lsp_config.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+
+    },
+    {
+      "mason-org/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "ts_ls",
+        },
+      },
+      dependencies = {
+          "mason-org/mason.nvim",
+          "neovim/nvim-lspconfig",
+      },
+    },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        local lspconfig = require("lspconfig")
+
+        lspconfig.lua_ls.setup({})
+        lspconfig.pyright.setup({})
+        lspconfig.ts_ls.setup({})
+      end
+    },
+}
