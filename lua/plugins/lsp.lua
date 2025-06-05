edfb87aa3d6e66fa604e8866126fe39ccb26fc29
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2025-06-05 10:49:12 -0300

    feat: added go and terraform ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,27 +5,29 @@ return {
 
     },
     {
-      "mason-org/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "ts_ls",
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "ts_ls",
+                "terraformls",
+                "gopls",
+            },
         },
-      },
-      dependencies = {
-          "mason-org/mason.nvim",
-          "neovim/nvim-lspconfig",
-      },
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
     },
     {
-      "neovim/nvim-lspconfig",
-      config = function()
-        local lspconfig = require("lspconfig")
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
 
-        lspconfig.lua_ls.setup({})
-        lspconfig.pyright.setup({})
-        lspconfig.ts_ls.setup({})
-      end
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.ts_ls.setup({})
+        end
     },
 }
