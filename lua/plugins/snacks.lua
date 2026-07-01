a97fdc0e909d1f54e8d1dcf74b1c8fb821a279de
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2026-07-01 08:52:10 -0300

    refactor: hoist picker exclude/hidden/ignored to top-level snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,20 +1,20 @@
+local excluded = { "node_modules", ".git" }
 return {
   "folke/snacks.nvim",
   opts = {
     picker = {
       layout = { preset = "select" }, -- use the telescope layout preset
+      exclude = excluded,
+      hidden = true,
+      ignored = true,
       sources = {
         files = {
           hidden = true,
-          exclude = { "node_modules", ".git" },
+          ignored = true,
         },
         grep = {
           hidden = true,
-          exclude = { "node_modules", ".git" },
-        },
-        explorer = {
-          hidden = true,
-          exclude = { "node_modules", ".git" },
+          ignored = true,
         },
         projects = {
           dev = { "~/dev/src" },
