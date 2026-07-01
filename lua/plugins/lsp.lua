3a44b4b1132d9fdb4e1b1aac56ac773ecc067057
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2026-07-01 08:52:00 -0300

    feat: add rubocop lsp server and set mason=false for ruby servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,8 +3,13 @@ return {
   opts = {
     servers = {
       ruby_lsp = {
+        mason = false,
         cmd = { os.getenv("HOME") .. "/.rbenv/shims/ruby-lsp" },
         cmd_env = { BUNDLE_GEMFILE = vim.fn.getenv("GLOBAL_GEMFILE") },
+      },
+      rubocop = {
+        mason = false,
+        cmd = { os.getenv("HOME") .. "/.rbenv/shims/rubocop" },
       },
     },
   },
