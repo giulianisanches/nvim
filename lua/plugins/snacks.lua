b32c9ba8a6f8bf4632ff23d964bb95d391140cbe
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2026-07-22 16:13:11 -0300

    chore: just a small reorg

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -22,21 +22,18 @@ return {
           max_depth = 4,
           confirm = function(picker, item)
             Snacks.picker.actions.load_session(picker, item)
+
             local all_bufs_ids = vim.api.nvim_list_bufs()
+
             local valid_bufs = vim.tbl_filter(function(buf)
               return vim.api.nvim_buf_is_valid(buf)
                 and vim.bo[buf].buftype == ""
                 and vim.api.nvim_buf_get_name(buf) ~= ""
             end, all_bufs_ids)
+
             local del_bufs = vim.tbl_filter(function(buf)
               return item.file ~= vim.fs.root(buf, ".git")
             end, valid_bufs)
-
-            picker:close()
-
-            for _, buf in ipairs(del_bufs) do
-              vim.api.nvim_buf_delete(buf, { force = true })
-            end
 
             local claudecode_loaded, terminal = pcall(require, "claudecode.terminal")
             local active_bufnr = claudecode_loaded
@@ -44,6 +41,12 @@ return {
               and terminal.get_active_terminal_bufnr()
             if active_bufnr then
               vim.cmd("ClaudeCodeSendText /quit")
+            end
+
+            picker:close()
+
+            for _, buf in ipairs(del_bufs) do
+              vim.api.nvim_buf_delete(buf, { force = true })
             end
           end,
         },
